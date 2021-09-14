c2633a564d48443e7f130fa12dc7a4dcfdf15607
byTurker <mr_turker@hotmail.com>
2021-09-14 17:23:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/coroutinesexample3/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.coroutinesexample3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Ths function takes 18 seconds but this doesn't work in parallel.
+        /*CoroutineScope(IO).launch {
+            Log.i("MyTag","Calculation started")
+            val stock1 = getStock1()
+            val stock2 = getStock2()
+            val total = stock1+stock2
+            Log.i("MyTag", "Total is $total")
+        }*/
+
+        //Ths function takes 9 seconds but this work in parallel.
+        /*CoroutineScope(IO).launch {
+            Log.i("MyTag","Calculation started")
+            val stock1 = async { getStock1() }
+            val stock2 = async { getStock2() }
+            val total = stock1.await() + stock2.await()
+            Log.i("MyTag", "Total is $total")
+        }*/
+
+        //Ths function takes 9 seconds but this work in parallel. This shows on log.
+        /*CoroutineScope(IO).launch {
+            Log.i("MyTag","Calculation started")
+            val stock1 = async { getStock1() }
+            val stock2 = async { getStock2() }
+            val total = stock1.await() + stock2.await()
+            Log.i("MyTag", "Total is $total")
+        }*/
+
+        //Ths function takes 9 seconds but this work in parallel. This shows on UI.
+        CoroutineScope(Main).launch {
+            Log.i("MyTag","Calculation started")
+            val stock1 = async(IO) { getStock1() }
+            val stock2 = async(IO) { getStock2() }
+            val total = stock1.await() + stock2.await()
+            Toast.makeText(applicationContext,"Total is $total",Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private suspend fun getStock1(): Int {
+        delay(10000)
+        Log.i("MyTag", " stock 1 returned!")
+        return 55000
+    }
+
+    private suspend fun getStock2(): Int {
+        delay(8000)
+        Log.i("MyTag", " stock 2 returned!")
+        return 35000
+    }
+}
